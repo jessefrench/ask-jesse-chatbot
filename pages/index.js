43ce0cb52b415939912aa6ad14ad0ae740e901d7
Jesse French <frejes32@gmail.com>
2023-02-17 15:25:30 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,56 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from "next/head"
+import { useState } from 'react'
 
-export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
-
-  async function onSubmit(event) {
-    event.preventDefault();
+export default function App() {
+  const [prompt, setPrompt] = useState('')
+  const [messages, setMessages] = useState([])
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setMessages([...messages, { prompt, user: 'human' }])
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
+      const response = await fetch('/api/generate', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ animal: animalInput }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+        body: JSON.stringify({ prompt }),
+      })
+      const data = await response.json()
+      setMessages(prevMessages => (
+         [...prevMessages, { prompt: data.message, user: 'ai' }]
+      ))
+      let chatWindow = document.getElementById('scroller')
+      let xH = chatWindow.scrollHeight
+      chatWindow.scrollTo(0, xH)
+    }
+    catch(error) {
+      console.log(error)
     }
   }
 
   return (
-    <div>
+    <div className="main">
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Ask Jesse</title>
+        <link rel="icon" href="/wave.png" />
       </Head>
-
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
+      <div className="logo" >
+        <img src="/logo.png" className="icon" />
+      </div>
+      <div className="message" id='scroller'>
+        {messages.map((msg, id) => (
+          <div key={id} className="person">
+            <img src={msg.user === 'human' ? '/thinking-face.png' : '/thumbs-up.png'} alt="icon" />
+            <p>
+              {msg.prompt}
+            </p>
+          </div>))
+        }
+        <div id="anchor"></div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setPrompt(e.target.value)} name="prompt" value={prompt} placeholder="Ask me something..." />
+      </form>
     </div>
-  );
+  )
 }
